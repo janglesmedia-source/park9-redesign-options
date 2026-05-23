66fef6724a0f44823708b83658c88eff2de236b0
janglesmedia-source <janglesmedia@gmail.com>
2026-05-23 15:34:38 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/park9-redesign-options/',
   plugins: [react(), tailwindcss()],
 })
